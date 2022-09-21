f58559137dd17be837b750b5a6de28cc5df44316
EvoX7 <creativesoultouch@gmail.com>
2022-09-21 17:20:42 +0200

    Update 1

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\User;
+use Faker\Generator as Faker;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        $personalUser = new User();
+        $personalUser->name = 'marc';
+        $personalUser->email = 'marc@gmail.com';
+        $personalUser->password = Hash::make ('unoduetrequattro5678');
+
+        $personalUser->save();
+
+
+        for ($i = 0; $i < 100; $i++) {
+            $user = new User();
+            $user->name = $faker->username();
+            $user->email = $faker->email();
+            $user->password = Hash::make ($faker->password());
+
+            $user->save();
+        }
+    }
+}
